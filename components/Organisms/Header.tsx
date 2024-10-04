d655d45926174395fb38b41989fb172af65be64b
Nicolas Osborne <nho.osborne@gmail.com>
2024-10-04 21:12:22 +0200

    Initial commit - Project Base Next.JS & TypeScript

---
+++ components/Organisms/Header.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import { FC } from 'react';
+import Menu from '~molecules/Menu';
+
+export interface HeaderProps {}
+
+const Header: FC<HeaderProps> = (props) => {
+
+  return (
+    <>
+      <Menu />
+
+      <div className="uam_blockHeader uam_blockHeader-selector">
+        <Image
+          className="uam_blockHeader_backgroundImage"
+          src="images/header.jpg"
+          alt='image de montagne'
+          width={1276}
+          height={675}
+          />
+        <div className="uam_container">
+          <div className="uam_blockHeader_container">
+            <div className="uam_blockHeader_label">
+              Mont revait
+              <div className="uam_icon-arrow" />
+            </div>
+            <div className="uam_blockHeader_content">
+              <div className="uam_blockHeader_title">
+                <div
+                  className="uam_blockHeaderTitle uam_blockHeaderTitle-home">
+                  <h1 className="uam_blockHeaderTitle_title">
+                    Bienvenue au mont revait
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Layout from "~template/Layout"
+
+interface HomePageProps {}
+
+
+const HomePage = (props: HomePageProps) => {
+
+  return (
+    <Layout>
+      Votre code ici
+    </Layout>
+  );
+}
+
+export default HomePage;
