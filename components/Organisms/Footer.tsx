5ad580981765b6f235be07272d3e35ab504a697b
Nicolas Osborne <nho.osborne@gmail.com>
2024-10-16 14:59:13 +0200

    Correct empty src for visaImageSrc in Footer.tsx

--- components/Organisms/Footer.tsx
+++ components/Organisms/Footer.tsx
@@ -1,55 +1,55 @@
-import Image from "next/image";
-import Link from "next/link";
-import { FC, useEffect, useLayoutEffect, useState } from "react";
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC, useEffect, useLayoutEffect, useState } from 'react'
 
 export interface FooterProps {}
 
 const Footer: FC<FooterProps> = () => {
-  const [visaImageSrc, setVisaImageSrc] = useState("");
-  const [windowWidth, setWindowWidth] = useState(0);
+  const [visaImageSrc, setVisaImageSrc] = useState('images/visa-card-2.png')
+  const [windowWidth, setWindowWidth] = useState(0)
 
   useEffect(() => {
     const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
+      setWindowWidth(window.innerWidth)
+    }
 
-    if (typeof window !== "undefined") {
-      setWindowWidth(window.innerWidth);
-      window.addEventListener("resize", handleResize);
+    if (typeof window !== 'undefined') {
+      setWindowWidth(window.innerWidth)
+      window.addEventListener('resize', handleResize)
     }
 
     return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("resize", handleResize);
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', handleResize)
       }
-    };
-  }, []);
+    }
+  }, [])
 
   useEffect(() => {
     if (windowWidth >= 740) {
-      setVisaImageSrc("images/visa-card-1.png");
+      setVisaImageSrc('images/visa-card-1.png')
     } else {
-      setVisaImageSrc("images/visa-card-2.png");
+      setVisaImageSrc('images/visa-card-2.png')
     }
-  }, [windowWidth]);
+  }, [windowWidth])
 
   return (
-    <div className="uam_footer">
-      <div className="uam_container">
-        <div className="uam_footer_container">
-          <div className="uam_footer_content">
-            <div className="uam_footer_information">
-              <div className="uam_footer_domaine">
-                <div className="uam_footerDomaine">
-                  <div className="uam_footerDomaine_logo">
+    <div className='uam_footer'>
+      <div className='uam_container'>
+        <div className='uam_footer_container'>
+          <div className='uam_footer_content'>
+            <div className='uam_footer_information'>
+              <div className='uam_footer_domaine'>
+                <div className='uam_footerDomaine'>
+                  <div className='uam_footerDomaine_logo'>
                     <Image
-                      src="images/Logo.svg"
-                      alt="logo blanc"
+                      src='images/Logo.svg'
+                      alt='logo blanc'
                       width={116}
                       height={50}
                     />
                   </div>
-                  <div className="uam_footerDomaine_description">
+                  <div className='uam_footerDomaine_description'>
                     <p>
                       <strong>Domaine Évasion Mont Revait</strong>, accès à
                       Saint-Michel, Saint-Nicolas, Courchevel, Montgenèvre -
@@ -58,37 +58,37 @@ const Footer: FC<FooterProps> = () => {
                   </div>
                 </div>
               </div>
-              <div className="uam_footer_payment">
-                <div className="uam_footerPayment">
-                  <h3 className="uam_footerPayment_title uam_footerTitle">
+              <div className='uam_footer_payment'>
+                <div className='uam_footerPayment'>
+                  <h3 className='uam_footerPayment_title uam_footerTitle'>
                     Moyens de paiement
                   </h3>
-                  <div className="uam_footerPayment_logoList">
+                  <div className='uam_footerPayment_logoList'>
                     <Image
-                      className="uam_footerPayment_logo"
-                      alt="visa"
+                      className='uam_footerPayment_logo'
+                      alt='visa'
                       src={visaImageSrc}
                       width={42}
                       height={30}
                     />
                     <Image
-                      className="uam_footerPayment_logo"
-                      src="images/mastercard.png"
-                      alt="master card"
+                      className='uam_footerPayment_logo'
+                      src='images/mastercard.png'
+                      alt='master card'
                       width={42}
                       height={30}
                     />
                     <Image
-                      className="uam_footerPayment_logo"
-                      src="images/american-express.png"
-                      alt="american express"
+                      className='uam_footerPayment_logo'
+                      src='images/american-express.png'
+                      alt='american express'
                       width={42}
                       height={30}
                     />
                     <Image
-                      className="uam_footerPayment_logo"
-                      src="images/paypal.png"
-                      alt="paypal"
+                      className='uam_footerPayment_logo'
+                      src='images/paypal.png'
+                      alt='paypal'
                       width={118}
                       height={30}
                     />
@@ -96,64 +96,64 @@ const Footer: FC<FooterProps> = () => {
                 </div>
               </div>
             </div>
-            <div className="uam_footer_links">
-              <div className="uam_footerLinks">
-                <h3 className="uam_footerLinks_title uam_footerTitle">
+            <div className='uam_footer_links'>
+              <div className='uam_footerLinks'>
+                <h3 className='uam_footerLinks_title uam_footerTitle'>
                   À propos
                 </h3>
-                <div className="uam_footerLinks_list">
-                  <ul className="uam_footerLinkList">
-                    <li className="uam_footerLinkList_item">
-                      <Link className="uam_footerLinkList_link" href="#">
+                <div className='uam_footerLinks_list'>
+                  <ul className='uam_footerLinkList'>
+                    <li className='uam_footerLinkList_item'>
+                      <Link className='uam_footerLinkList_link' href='#'>
                         Réclamations
                       </Link>
                     </li>
-                    <li className="uam_footerLinkList_item">
-                      <Link className="uam_footerLinkList_link" href="#">
+                    <li className='uam_footerLinkList_item'>
+                      <Link className='uam_footerLinkList_link' href='#'>
                         Points de retrait
                       </Link>
                     </li>
-                    <li className="uam_footerLinkList_item">
-                      <Link className="uam_footerLinkList_link" href="#">
+                    <li className='uam_footerLinkList_item'>
+                      <Link className='uam_footerLinkList_link' href='#'>
                         Domaine skiable
                       </Link>
                     </li>
-                    <li className="uam_footerLinkList_item">
-                      <Link className="uam_footerLinkList_link" href="#">
+                    <li className='uam_footerLinkList_item'>
+                      <Link className='uam_footerLinkList_link' href='#'>
                         Programme de fidélité
                       </Link>
                     </li>
-                    <li className="uam_footerLinkList_item">
-                      <Link className="uam_footerLinkList_link" href="#">
+                    <li className='uam_footerLinkList_item'>
+                      <Link className='uam_footerLinkList_link' href='#'>
                         Office de tourisme
                       </Link>
                     </li>
                   </ul>
                 </div>
               </div>
-              <div className="uam_footerLinks">
-                <h3 className="uam_footerLinks_title uam_footerTitle">
+              <div className='uam_footerLinks'>
+                <h3 className='uam_footerLinks_title uam_footerTitle'>
                   Informations
                 </h3>
-                <div className="uam_footerLinks_list">
-                  <ul className="uam_footerLinkList">
-                    <li className="uam_footerLinkList_item">
-                      <Link className="uam_footerLinkList_link" href="#">
+                <div className='uam_footerLinks_list'>
+                  <ul className='uam_footerLinkList'>
+                    <li className='uam_footerLinkList_item'>
+                      <Link className='uam_footerLinkList_link' href='#'>
                         Conditions générales
                       </Link>
                     </li>
-                    <li className="uam_footerLinkList_item">
-                      <Link className="uam_footerLinkList_link" href="#">
+                    <li className='uam_footerLinkList_item'>
+                      <Link className='uam_footerLinkList_link' href='#'>
                         Kit de presse
                       </Link>
                     </li>
-                    <li className="uam_footerLinkList_item">
-                      <Link className="uam_footerLinkList_link" href="#">
+                    <li className='uam_footerLinkList_item'>
+                      <Link className='uam_footerLinkList_link' href='#'>
                         Support
                       </Link>
                     </li>
-                    <li className="uam_footerLinkList_item">
-                      <Link className="uam_footerLinkList_link" href="#">
+                    <li className='uam_footerLinkList_item'>
+                      <Link className='uam_footerLinkList_link' href='#'>
                         Mentions légales
                       </Link>
                     </li>
@@ -162,28 +162,28 @@ const Footer: FC<FooterProps> = () => {
               </div>
             </div>
           </div>
-          <div className="uam_footer_footer">
-            <div className="uam_footerFooter">
-              <div className="uam_footerFooter_socialNetworksLinks">
-                <Link href="#">
-                  <i className="site-facebook"></i>
+          <div className='uam_footer_footer'>
+            <div className='uam_footerFooter'>
+              <div className='uam_footerFooter_socialNetworksLinks'>
+                <Link href='#'>
+                  <i className='site-facebook'></i>
                 </Link>
-                <Link href="#">
-                  <i className="site-Instagram"></i>
+                <Link href='#'>
+                  <i className='site-Instagram'></i>
                 </Link>
-                <Link href="#">
-                  <i className="site-LinkedIn"></i>
+                <Link href='#'>
+                  <i className='site-LinkedIn'></i>
                 </Link>
-                <Link href="#">
-                  <i className="site-Twitter"></i>
+                <Link href='#'>
+                  <i className='site-Twitter'></i>
                 </Link>
-                <Link href="#">
-                  <i className="site-YouTube"></i>
+                <Link href='#'>
+                  <i className='site-YouTube'></i>
                 </Link>
               </div>
-              <div className="uam_footerFooter_line"></div>
-              <div className="uam_footerFooter_copyright">
-                <span className="js-cookie extraBottom-cookie">
+              <div className='uam_footerFooter_line'></div>
+              <div className='uam_footerFooter_copyright'>
+                <span className='js-cookie extraBottom-cookie'>
                   Paramétrez vos cookies
                 </span>
                 <span> Made with ♡ by UAM 2022 </span>
@@ -192,9 +192,9 @@ const Footer: FC<FooterProps> = () => {
           </div>
         </div>
       </div>
-      <div className="uam_footer_triangle"></div>
+      <div className='uam_footer_triangle'></div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
